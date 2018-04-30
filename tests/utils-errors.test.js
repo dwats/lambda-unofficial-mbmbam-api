373aa268794d4cd0abbf56037314f28844d7bf1e
Eric Julius <ejulius86@gmail.com>
2018-04-29 19:01:33 -0500

    testing

---
+++ tests/utils-errors.test.js
@@ -0,0 +1,5 @@
+const chai = require('chai')
+
+const translateError = require('../utils/error')
+
+const should = chai.should()
---
+++ tests/utils-response.test.js
@@ -0,0 +1,5 @@
+const chai = require('chai')
+
+const respone = require('../utils/response')
+
+const should = chai.should()
