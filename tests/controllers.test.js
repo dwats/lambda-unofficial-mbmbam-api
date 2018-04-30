dae85a3e10adfc1fb0b428251b53979640ae7d03
Eric Julius <ejulius86@gmail.com>
2018-04-29 19:01:41 -0500

    testing

---
+++ tests/controllers.test.js
@@ -0,0 +1,6 @@
+const chai = require('chai')
+
+const handleGetEpisodes = require('../controllers/handleGetEpisodes')
+const handleGetLatestEpisode = require('../controllers/handleGetLatestEpisodes')
+
+const should = chai.should()
---
+++ tests/handler.test.js
@@ -0,0 +1,5 @@
+const chai = require('chai')
+
+const { getEpisodes, getLatestEpisode } = require('../handler')
+
+const should = chai.should()
