5bb0fd8052a857c1affa9183cbc519446fa9ffcf
Eric Julius <ejulius86@gmail.com>
2018-05-01 00:13:22 -0500

    Added mock functions for lambdaInvoker and translateError

--- tests/seed/utils.js
+++ tests/seed/utils.js
@@ -1,30 +1,35 @@
 const fs = require('fs-extra')
 const path = require('path')
-const convert = require('xml-js')
 
 async function mockRequest () {
   return fs.readFile(path.join(__dirname, 'data.xml'), 'utf-8')
 }
 
-const mockRequestReject = async () => Promise.reject('test reject')
-
-async function mockCacheWarmer () {
-  const options = {
-    compact: true,
-    textKey: 'text',
-    cdataKey: 'cdata',
-    attributesKey: 'attributes'
-  }
-
-  const xml = await mockRequest()
-  const cache = convert.xml2js(xml, options)
-  cache.rss.channel.item = [...cache.rss.channel.item.reverse()]
-
-  return cache
+async function mockRequestReject () {
+  return Promise.reject(Error('test reject'))
 }
+
+async function mockLambdaInvokerSuccess () {
+  return Promise.resolve('test success')
+}
+
+async function mockLambdaInvokerError () {
+  return Promise.reject(Error('test error'))
+}
+
+function mockTranslateError (msg) {
+  const newErr = new Error(msg)
+  newErr.originalError = msg
+  return e => {
+    throw newErr
+  }
+}
+
 
 module.exports = {
   mockRequest,
   mockRequestReject,
-  mockCacheWarmer
+  mockLambdaInvokerSuccess,
+  mockLambdaInvokerError,
+  mockTranslateError
 }
